7d8f582f177f4a0f3d4ddd143d194cf3bed261dc
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-02-05 16:20:04 -0600

    Switch from an ssh-based to a unix sockets-based method
    Xpra has several modes how a client can connect to a server. The most
    common is ssh because it provides very decent encryption without much
    hassle between a client machine and a host machine.
    
    However, in docker-desktop's scenario, the host machine is actually
    running on the very same machine. So unix sockets are a much better
    alternative because they provide security based on the file system,
    and they are easier to set up (apart from having to start the Docker
    container via sudo, this method does not require sudo because we
    do not have to figure out which port the sshd is redirected to).
    
    This also opens the door for "throw-away" containers, i.e. containers
    whose sole purpose is to live as long as a given application is running
    and after that be safely forgotten, with all its intermediate files.
    
    Also, by making it very easy to have multiple containers, all running
    an Xpra server inside, it is much easier to insulate applications,
    or even different sessions of the same application, from one another.
    Think e.g. surfing to a website whose advertisers read your Facebook
    cookies.
    
    These unix sockets can be accessed via ssh from a remote machine, too:
    
    	REMOTE_SOCAT='socat - UNIX:docker-desktop/shared/docker-desktop-10'
    	socat UNIX-LISTEN:$(hostname)-10 \
    		EXEC:"ssh host-machine '$REMOTE_SOCAT'" &
    	xpra --socket-dir=$(pwd) attach :10
    
    In plain English: this uses a local and a remote socat, connected via ssh,
    to allow a local Xpra to connect to the remote Docker container's Xpra.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- connect.sh
+++ connect.sh
@@ -9,12 +9,12 @@ die () {
 }
 
 image=dscho/docker-desktop
-container="$(sudo docker ps |
-	sed -n "s|^\([^ ]*\) *$image:latest .*|\1|p")"
 
 # Make sure that we have a public ssh key in shared/
 test -f shared/id_rsa.pub || {
 	echo "Need to generate a private/public ssh key pair" >&2
+	container="$(sudo docker ps |
+		sed -n "s|^\([^ ]*\) *$image:latest .*|\1|p")"
 	test -z "$container" ||
 	! echo "Stopping container $container" >&2 ||
 	sudo docker stop "$container" ||
@@ -23,22 +23,18 @@ test -f shared/id_rsa.pub || {
 	ssh-keygen -b 4096 -f id_rsa -N '' &&
 	mv id_rsa.pub shared/ ||
 	die "Could not generate/copy private key"
-	container=
 }
 
 # Make sure that docker-desktop is running
-test -n "$container" ||
-container="$(sudo docker run -h=docker-desktop \
-	-v "$(pwd)"/shared/:/shared -P -t -i -d "$image")" ||
+test -S shared/docker-desktop-10 || {
+	sudo docker run -h=docker-desktop \
+		-v "$(pwd)"/shared/:/shared -P -t -i -d "$image" &&
+	# Give the container some time to start up, like, one second
+	sleep 2
+} ||
 die "Could not run $image"
 
-# Determine port on which ssh is running
-ssh_port="$(sudo docker port "$container" 22)"
-ssh_port=${ssh_port##*:}
-
-# Give the container some time to start up, like, one second
-sleep 2
 
 # Call xpra
-xpra --ssh="ssh -o NoHostAuthenticationForLocalhost=true -i \"$(pwd)\"/id_rsa docker@127.0.0.1 -p $ssh_port" \
-	attach ssh::10
+socat UNIX-LISTEN:shared/$(hostname)-10 UNIX:shared/docker-desktop-10 &
+xpra --socket-dir="$(pwd)"/shared/ attach :10
