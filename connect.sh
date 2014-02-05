b4768e7a7afc356393ba2ec283e5f7113adc5375
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-02-04 22:00:32 -0600

    Use a better way to get authorized_keys installed
    The previous attempt to commit the public ssh key into the image was
    misguided: it asked for over-sharing of information.
    
    Instead, let's just pick up the public key from the shared/ directory and
    install it into $HOME/.ssh/authorized_keys upon container startup (via
    docker-desktop).
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- connect.sh
+++ connect.sh
@@ -12,8 +12,8 @@ image=dscho/docker-desktop
 container="$(sudo docker ps |
 	sed -n "s|^\([^ ]*\) *$image:latest .*|\1|p")"
 
-# Make sure that we have an id_rsa file
-test -f id_rsa || {
+# Make sure that we have a public ssh key in shared/
+test -f shared/id_rsa.pub || {
 	echo "Need to generate a private/public ssh key pair" >&2
 	test -z "$container" ||
 	! echo "Stopping container $container" >&2 ||
@@ -21,19 +21,14 @@ test -f id_rsa || {
 	die "Could not stop container $container; need to make an ssh key"
 
 	ssh-keygen -b 4096 -f id_rsa -N '' &&
-	cat id_rsa.pub |
-	sudo docker run -cidfile=tmp.id "$image" \
-		tee /home/docker/.ssh/authorized_keys &&
-	container="$(cat tmp.id)" &&
-	rm -f tmp.id &&
-	sudo docker commit "$container" "$image" ||
-	die "Could not insert private key into container"
+	mv id_rsa.pub shared/ ||
+	die "Could not generate/copy private key"
 	container=
 }
 
 # Make sure that docker-desktop is running
 test -n "$container" ||
-container="$(sudo docker run -h=docker-desktop -P -d "$image")" ||
+container="$(sudo docker run -h=docker-desktop -v "$(pwd)"/shared/:/shared -P -d "$image")" ||
 die "Could not run $image"
 
 # Determine port on which ssh is running
